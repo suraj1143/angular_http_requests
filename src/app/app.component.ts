c5991005401d709b08f8d13131e048bab10cd268
suraj gaikwad <surajgaikwad284@gmail.com>
2022-05-06 23:08:15 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http';
-}
+export class AppComponent implements OnInit {
+  loadedPosts:Post[] = [];
+  isFetching = false ; 
+
+  constructor(private http:HttpClient, private postsService : PostsService ) {}
+
+  ngOnInit() {
+    this.postsService.fetchPosts();
+  }
+
+  onCreatePost(postData:Post){
+    //send http request
+    this.postsService.createAndStorePost(postData.title , postData.content);
+  }
+
+  onFetchPosts() {
+    //send http request
+    this.postsService.fetchPosts();
+  }
+
+  onClearPosts() {
+    //send http request
+  }
+  
+  }
+
